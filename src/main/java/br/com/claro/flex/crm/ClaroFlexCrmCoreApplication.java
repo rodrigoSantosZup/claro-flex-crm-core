2e4139c87a71242a0629ac41409201823918bb2c
Rodrigo Costa <santos.rodrigo@zup.com.br>
2023-12-19 13:48:30 -0300

    first commit

---
+++ src/main/java/br/com/claro/flex/crm/ClaroFlexCrmCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.claro.flex.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroFlexCrmCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroFlexCrmCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/claro/flex/crm/ClaroFlexCrmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claro.flex.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroFlexCrmCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
